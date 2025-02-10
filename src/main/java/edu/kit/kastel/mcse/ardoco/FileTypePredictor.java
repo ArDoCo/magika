33bd0daa75053231eaff64c919c3087a9349b8c2
Jan Keim <jan.keim@kit.edu>
2025-02-10 15:48:25 +0100

    Improve Predictor code

--- src/main/java/edu/kit/kastel/mcse/ardoco/FileTypePredictor.java
+++ src/main/java/edu/kit/kastel/mcse/ardoco/FileTypePredictor.java
@@ -3,16 +3,14 @@ package edu.kit.kastel.mcse.ardoco;
 import ai.onnxruntime.*;
 
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.RandomAccessFile;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.*;
 import java.util.logging.Logger;
 import java.util.stream.Collectors;
-import java.util.stream.IntStream;
-import java.util.stream.Stream;
 
 public class FileTypePredictor {
     private static final Logger logger = Logger.getLogger(FileTypePredictor.class.getName());
@@ -46,39 +44,39 @@ public class FileTypePredictor {
             logger.warning("Input file path does not exist: " + modelPath.toAbsolutePath());
         }
 
-
         var config = new Configuration();
-        var fullInput = readFileToBuffer(inputFilePath.toFile());
-        int[][] inputBuffer = createInputBuffer(fullInput,config);
+        int[][] inputBuffer = readFileToInputBuffer(inputFilePath.toFile(), config);
+        return getPrediction(inputBuffer);
+    }
 
+    private Prediction getPrediction(int[][] inputBuffer) {
         var env = OrtEnvironment.getEnvironment();
-        try (OrtSession.SessionOptions opts = new OrtSession.SessionOptions();
-             var session = env.createSession(modelPath.toAbsolutePath().toString(),opts)){
+        try (OrtSession.SessionOptions opts = new OrtSession.SessionOptions(); var session = env.createSession(modelPath.toAbsolutePath().toString(), opts)) {
 
             String inputName = session.getInputNames().iterator().next();
             var tensor = OnnxTensor.createTensor(env, inputBuffer);
-            var result = session.run(Collections.singletonMap(inputName,tensor));
-            float[][] outputProbs = (float[][]) result.get(0).getValue();
-            int labelIndex = predict(outputProbs[0]);
+            var result = session.run(Collections.singletonMap(inputName, tensor));
+            float[][] outputProbabilities = (float[][]) result.get(0).getValue();
+            int labelIndex = predict(outputProbabilities[0]);
 
-            return new Prediction(labels.get(labelIndex), outputProbs[0][labelIndex]);
+            return new Prediction(labels.get(labelIndex), outputProbabilities[0][labelIndex]);
         } catch (OrtException e) {
             throw new RuntimeException(e);
         }
     }
 
-    public Map<Path,Prediction> predictFileTypes(Collection<Path> inputFiles) {
+    public Map<Path, Prediction> predictFileTypes(Collection<Path> inputFiles) {
         Map<Path, Prediction> predictions = new HashMap<>();
         for (var inputFile : inputFiles) {
             if (!Files.isDirectory(inputFile)) {
                 var prediction = predictFileType(inputFile);
-                predictions.put(inputFile,prediction);
+                predictions.put(inputFile, prediction);
             }
         }
         return predictions;
     }
 
-    public Map<Path,Prediction> predictFileTypesFromFolderRecursively(Path inputFolder) {
+    public Map<Path, Prediction> predictFileTypesFromFolderRecursively(Path inputFolder) {
         checkFolder(inputFolder);
 
         Set<Path> files = findAllFilesRecursively(inputFolder);
@@ -96,7 +94,7 @@ public class FileTypePredictor {
         }
     }
 
-    public Map<Path,Prediction> predictFileTypesFromFolder(Path inputFolder) {
+    public Map<Path, Prediction> predictFileTypesFromFolder(Path inputFolder) {
         checkFolder(inputFolder);
 
         Set<Path> files = findAllFilesRecursively(inputFolder, 1);
@@ -113,51 +111,6 @@ public class FileTypePredictor {
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-    }
-
-    private static int[][] createInputBuffer(byte[] fullInput, Configuration config) {
-        int beginningSize = config.getBeginningSize();
-        int midSize = config.getMidSize();
-        int endSize = config.getEndSize();
-        var bufferSize = beginningSize + midSize + endSize;
-        int paddingToken = config.getPaddingToken();
-
-        int[] beginningArray = new int[beginningSize];
-        for (int i = 0; i < beginningArray.length; i++) {
-            if (i < fullInput.length) {
-                beginningArray[i] = fullInput[i];
-            } else {
-                beginningArray[i] = paddingToken;
-            }
-        }
-
-        int[] midArray = new int[midSize];
-        int halfInputSize = Math.round(fullInput.length / 2);
-        int startHalf = Math.max(0, halfInputSize - (midSize / 2));
-        for (int i = 0; i < midArray.length; i++) {
-            int inputIndex = startHalf + i;
-            if (i < fullInput.length) {
-                midArray[i] = fullInput[inputIndex];
-            } else {
-                midArray[i] = paddingToken;
-            }
-        }
-
-        int[] endArray = new int[endSize];
-        for (int i = 1; i <= endArray.length; i++) {
-            int inputIndex = fullInput.length - i;
-            int endArrayIndex = endArray.length - i;
-            if (i <= fullInput.length) {
-                endArray[endArrayIndex] = fullInput[inputIndex];
-            } else {
-                endArray[endArrayIndex] = paddingToken;
-            }
-        }
-
-        int[] inputArray = IntStream.concat(IntStream.concat(Arrays.stream(beginningArray), Arrays.stream(midArray)), Arrays.stream(endArray)).toArray();
-        int[][] returnArray = new int[1][bufferSize];
-        returnArray[0] = inputArray;
-        return returnArray;
     }
 
     /**
@@ -178,14 +131,71 @@ public class FileTypePredictor {
         return index;
     }
 
-    private static byte[] readFileToBuffer(File file) {
-        byte[] byteArray = new byte[(int) file.length()];
-        try (FileInputStream inputStream = new FileInputStream(file)) {
-            inputStream.read(byteArray);
+    private static int[][] readFileToInputBuffer(File file, Configuration config) {
+        long fileLength = file.length();
+
+        int beginningSize = config.getBeginningSize();
+        int midSize = config.getMidSize();
+        int endSize = config.getEndSize();
+        var bufferSize = beginningSize + midSize + endSize;
+        byte paddingToken = (byte) config.getPaddingToken();
+
+        byte[] beginningArray = new byte[beginningSize];
+        Arrays.fill(beginningArray, paddingToken);
+
+        byte[] midArray = new byte[midSize];
+        Arrays.fill(midArray, paddingToken);
+
+        int endByteArraySize = Math.min((int) fileLength, endSize);
+        byte[] endArray = new byte[endByteArraySize];
+        Arrays.fill(endArray, paddingToken);
+
+        try (RandomAccessFile raf = new RandomAccessFile(file, "r")) {
+
+            raf.read(beginningArray);
+
+            int offset = 0;
+            if (midSize > 0) {
+                int halfInputSize = Math.round((float) fileLength / 2);
+                offset = Math.max(0, halfInputSize - (midSize / 2));
+                raf.seek(offset);
+                raf.read(midArray);
+            }
+
+            offset = (int) Math.max(0, fileLength - endSize);
+            raf.seek(offset);
+            raf.read(endArray);
+            if (endSize > endByteArraySize) {
+                byte[] tmp = new byte[endSize];
+                Arrays.fill(tmp, paddingToken);
+                for (int i = 1; i <= endArray.length; i++) {
+                    int indexEnd = endArray.length - i;
+                    int indexTmp = tmp.length - i;
+                    tmp[indexTmp] = endArray[indexEnd];
+                }
+                endArray = tmp;
+            }
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-        return byteArray;
+
+        int[] inputArray = new int[beginningSize + midSize + endSize];
+        int maxSize = Math.max(Math.max(beginningSize, midSize), endSize);
+        for (int i = 0; i < maxSize; i++) {
+            if (i < beginningSize) {
+                inputArray[i] = beginningArray[i];
+            }
+            if (i < midSize) {
+                inputArray[i + beginningSize] = midArray[i];
+            }
+            if (i < endSize) {
+                inputArray[i + beginningSize + midSize] = endArray[i];
+            }
+        }
+
+        int[][] returnArray = new int[1][bufferSize];
+        returnArray[0] = inputArray;
+        return returnArray;
     }
 
     private static void printOutputProbs(float[][] outputProbs) {
